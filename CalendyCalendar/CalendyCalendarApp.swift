f9555b8246fdfdf3a960bd4040cc8aa802dd22da
Sasha <6260727+sashimi0@users.noreply.github.com>
2025-07-03 20:32:45 -0700

    Initial Commit

---
+++ CalendyCalendar/CalendyCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendyCalendarApp.swift
+//  CalendyCalendar
+//
+//  Created by sasha on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendyCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendyCalendarTests/CalendyCalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalendyCalendarTests.swift
+//  CalendyCalendarTests
+//
+//  Created by sasha on 7/3/25.
+//
+
+import Testing
+@testable import CalendyCalendar
+
+struct CalendyCalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
